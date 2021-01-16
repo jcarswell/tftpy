d904d548e0e07a48722a8b00bac28c0a4ae945d0
Josh Carswell <josh.carswell@thecarswells.ca>
2021-01-16 08:20:04 -0600

    Updated docstrings and added annotations

--- tftpy/context/server.py
+++ tftpy/context/server.py
@@ -1,6 +1,10 @@
 import logging
 import time
 
+from io import IOBase
+from typing import Callable
+from typing import Any
+
 from .base import Context
 from tftpy.states import Start
 
@@ -9,7 +13,20 @@ logger = logging.getLogger('tftpy.context.server')
 class Server(Context):
     """The context for the server."""
     
-    def __init__(self, host, port, timeout, root, dyn_file_func=None, upload_open=None, **kwargs):
+    def __init__(self, host: str, port: int, timeout: int, root: str, 
+                 dyn_file_func: Callable[[str,str,int],Any] = None,
+                 upload_open: Callable[[str,Context],IOBase]=None,
+                 **kwargs) -> None:
+        """Prepare the server context to process data from a client
+
+        Args:
+            host (str): The requesting clients IP
+            port (int): The requesting clients Port
+            timeout (int): stocket timeout
+            root (str): server root path
+            dyn_file_func (Callable[[str,str,int],Any], optional): A dynamic fucntion function to read from
+            upload_open (Callable[[str,Context],IOBase], optional): A dynamic function to write data to
+        """
         
         super().__init__(host, port, timeout, **kwargs)
         
@@ -21,11 +38,16 @@ class Server(Context):
         self.dyn_file_func = dyn_file_func
         self.upload_open = upload_open
 
-    def start(self, buffer=None):
+    def start(self, buffer: bytes = None) -> None:
         """Start the state cycle. Note that the server context receives an
         initial packet in its start method. Also note that the server does not
         loop on cycle(), as it expects the TftpServer object to manage
-        that."""
+        that.
+
+        Args:
+            buffer (bytes, optional): Buffer Data receivied from the client.
+                Should be either a read or write request
+        """
         
         logger.debug("In tftpy.contex.server.start")
         self.metrics.start_time = self.last_update = time.time()
@@ -37,7 +59,7 @@ class Server(Context):
         # the download or the upload state.
         self.state = self.state.handle(pkt,self.host,self.port)
 
-    def end(self, *args, **kwargs):
+    def end(self, *args, **kwargs) -> None:
         """Finish up the context."""
         
         super().end(*args, **kwargs)
