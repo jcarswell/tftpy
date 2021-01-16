253baf91dd7d25e8ff53bb7a5450ef0990783bf7
Josh Carswell <josh.carswell@thecarswells.ca>
2021-01-16 08:26:59 -0600

    Missed a few f-strings. Updated Docstrings and added annotation

--- tftpy/packet/types/data.py
+++ tftpy/packet/types/data.py
@@ -12,42 +12,58 @@ class Data(TftpPacket):
     DATA  | 03    | Block # | Data  |
            ---------------------~~--
     """
-    def __init__(self):
+    
+    def __init__(self) -> None:
         super().__init__()
         self.opcode = 3
         self.blocknumber = 0
         self.data = None
 
-    def __str__(self):
-        s = 'DAT packet: block %s' % self.blocknumber
+    def __str__(self) -> str:
+        s = f"DAT packet: block {self.blocknumber}"
         if self.data:
-            s += '\n    data: %d bytes' % len(self.data)
+            s += f"\n    data: {len(self.data)} bytes"
+
         return s
 
-    def encode(self):
-        """Encode the DAT packet. This method populates self.buffer, and
-        returns self for easy method chaining."""
+    def encode(self) -> 'Data':
+        """Encode the Data packet.
+
+        Returns:
+            Data: self
+        """
+
         if len(self.data) == 0:
             logger.debug("Encoding an empty DAT packet")
+
         data = self.data
+
         if not isinstance(self.data, bytes):
             data = self.data.encode('ascii')
+
         fmt = b"!HH%ds" % len(data)
         self.buffer = struct.pack(fmt,
                                   self.opcode,
                                   self.blocknumber,
                                   data)
+
         return self
 
-    def decode(self):
-        """Decode self.buffer into instance variables. It returns self for
-        easy method chaining."""
+    def decode(self) -> 'Data':
+        """Decode Data packet.
+
+        Returns:
+            Data: self
+        """
+
         # We know the first 2 bytes are the opcode. The second two are the
         # block number.
         (self.blocknumber,) = struct.unpack(str("!H"), self.buffer[2:4])
-        logger.debug("decoding DAT packet, block number %d", self.blocknumber)
-        logger.debug("should be %d bytes in the packet total", len(self.buffer))
+        logger.debug(f"decoding DAT packet, block number {self.blocknumber}")
+        logger.debug(f"should be {len(self.buffer)} bytes in the packet total")
         # Everything else is data.
+        
         self.data = self.buffer[4:]
-        logger.debug("found %d bytes of data", len(self.data))
+        logger.debug(f"found {len(self.data)} bytes of data")
+        
         return self
